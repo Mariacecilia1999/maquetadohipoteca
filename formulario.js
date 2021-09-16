2934461ded3cad9145dc077badaa078481e8cbeb
Mariacecilia1999 <mariaceciliaconti1999@gmail.com>
2021-09-16 17:59:58 -0300

    agregando algunos cambios

--- formulario.js
+++ formulario.js
@@ -1,20 +1,45 @@
 
- document.getElementById("contacto").style.display="none";
+document.getElementById("contacto").style.display="none";
 
 
 
 function enviar(){
 
-    document.getElementById("formulario").style.display="none";
+    
     
     var nombre=document.getElementById("nombre").value;
     var apellido=document.getElementById("apellido").value;
     var mail=document.getElementById("mail").value;
+
+    if(nombre < 3 || apellido < 3 || mail.length < 3){
+        document.getElementById("cambiarTitulo").innerHTML="Error al enviar el mensaje"
+        document.getElementById("cambiandoParrafo").innerHTML="Por favor completa el formulario de nuevo"
+    }
+
+
     
-    
-    document.getElementById("llama").style.display="none";
-    document.getElementById("contacto").style.display="block";
-    
-    document.getElementById("contacto").innerHTML= nombre+" "+apellido+" muchas gracias por contactarte con nosotros, pronto te contactaremos a tu mail: "+mail ;
+    else{
+            
+        document.getElementById("cambiarTitulo").innerHTML="Mensaje enviado"
+        document.getElementById("cambiandoParrafo").innerHTML=nombre+" "+apellido+" muchas gracias por contactarte con nosotros "
+        document.getElementById("cambiandoANombre").innerHTML="Pronto nos comunicaremos al mail ingresado: "+mail;
+
+        document.getElementById("bloqueoFormulario").style.display="none";
+        document.getElementById("ocultar").style.display="none";
+        }
+
 }
 
+$(document).ready(function(){
+    $("#boton").click(function(){
+        $(".mostrarMain").show(3000)
+    })
+})
+
+$(document).ready(function(){
+    $(".ancla").click(function(evento){
+      evento.preventDefault();
+      var codigo = "#" + $(this).data("ancla");
+      $("html,body").animate({scrollTop: $(codigo).offset().top}, 3000);
+    });
+  });
